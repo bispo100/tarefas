2133c6e4892eb4f48052c68ea7304c351e658e78
bispo100 <jromualdob2@gmail.com>
2025-05-05 11:32:20 -0300

    modificações button
    adicionamos botoes.

--- tarefas.js
+++ tarefas.js
@@ -4,16 +4,37 @@ let $erro_msg = document.getElementById("erro_msg")
 let $tarefas_lista = document.getElementById("tarefas_lista")
 
 function AdicionarTarefa(){
+    $erro_msg.textContent = ""
     let tarefa_txt = $tarefa_input.value.trim()
 
-    //criando elementos
-    let $tarefa_item = document.createElement("li")
-    
-    //adicionando conteudo no elemento
-    $tarefa_item.innerText =tarefa_txt
+    if(tarefa_txt !==""){
 
-    //adicionar o elemento na pagina
-    $tarefas_lista.appendChild($tarefa_item)
-    $tarefa_input.value = ""
+        //criando elementos
+        let $tarefa_item = document.createElement("li")
+        let $btn_completar =document.createElement("button")
+        let $btn_deletar =document.createElement("button")
+        //adicionando conteudo no elemento
+        $tarefa_item.innerText =tarefa_txt
+        $btn_completar.innerText = "completar"
+        $btn_deletar.innerText = "deletar"
+
+        //adicionar o elemento na pagina
+        $tarefa_item.appendChild($btn_completar)
+        $tarefa_item.appendChild($btn_deletar)
+        $tarefas_lista.appendChild($tarefa_item)
+        
+        //completar
+        $btn_completar.addEventListener('click', () => $tarefa_item.classList.add("colorir"))
+
+        //Deletar
+        $btn_deletar.addEventListener('click', () => $tarefa_item.remove())
+
+
+        $tarefa_input.value = ""
+
+    } else {
+        $erro_msg.textContent = "Digite uma tarefa!"
+
+    }
 
 }
